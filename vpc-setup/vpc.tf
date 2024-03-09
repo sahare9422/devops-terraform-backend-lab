254cf40d085fd6617c5b3299a4557c46cabf0eb4
Deepak Kumar <128142357+devops-aws-geek@users.noreply.github.com>
2024-03-09 11:48:50 +0530

    Update vpc.tf

--- vpc-setup/vpc.tf
+++ vpc-setup/vpc.tf
@@ -1,12 +1,4 @@
 # Create VPC/Subnet/Security Group/Network ACL
-provider "aws" {
-  version = "~> 2.0"
-  access_key = var.access_key 
-  secret_key = var.secret_key 
-  region     = var.region
-}
-
-
 #create the VPC
 resource "aws_vpc" "My_VPC" {
   cidr_block           = var.vpcCIDRblock
